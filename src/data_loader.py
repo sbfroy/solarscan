14d4a4461cec63a7b33417af908b231219790db4
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-10-05 11:41:40 +0200

    Added resize

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,8 @@
 import cv2
 
+IMG_SIZE = (768, 768)
 
 def image_loader(img_path):
     image = cv2.imread(img_path)
-    return image
+    resized_image = cv2.resize(image, IMG_SIZE)
+    return resized_image

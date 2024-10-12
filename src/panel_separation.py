f655a8066d3bcaa7d267cffb5371af4f305091e6
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-10-12 13:17:25 +0200

    working panel serparator

--- src/panel_separation.py
+++ src/panel_separation.py
@@ -3,39 +3,78 @@ import cv2
 
 from data_loader import single_image_loader
 from preprocessing import prep
+from config import IMG_SIZE
 
 
-def detect_edges(image):
-    # gradient intensities below threshold1 are discarded
-    # values above threshold 2 is definitely an edge
-    median = np.median(image)
-    lower = int(max(0, 0.33 * median))
-    upper = int(min(255, 1.6 * median))
-    edges = cv2.Canny(image, threshold1=lower, threshold2=upper)
-    return edges
+def detect_edges(preprocessed_image):
+    """
+    Detects edges in the preprocessed image using Canny edge detection.
 
-def enhance_edges(edges):
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    dilated = cv2.dilate(edges, kernel, iterations=1)
-    return dilated
+    Args:
+        preprocessed_image (): 
+
+    Returns: 
+        numpy.ndarray: binary image with edges (255) detected.
+    """
+
+    median = np.median(preprocessed_image)
+    lower = int(max(0, 0.66 * median)) # gradient intensities below lower are discarded
+    upper = int(min(255, 1.33 * median)) # values above upper is definitely an edge
+
+    edges = cv2.Canny(preprocessed_image, threshold1=lower, threshold2=upper)
+
+    kernel = np.ones((5, 5), np.uint8) # kernel size = 5x5
+    closed_edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel) # closes gaps in between edges
+
+    return closed_edges
 
 def find_contours(edges, image):
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-    cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
-    return image
+    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+    return contours
+    
+def isolate_panel(image):
+    """
+    The pipeline for panel isolation.
+
+    Args: 
+        image (): Unprocessed image from data_loader.py
+
+    Returns:
+        numpy.ndarray: an adjustable array that reflects how many and the location of panels. 
+        along with info about the panel cluster.
+
+    """
+    preprocessed_image = prep(image)
+    edges = detect_edges(preprocessed_image)
+    contours = find_contours(edges, image)
+
+    panel = None
+    biggest_area = 0
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 100000:
+            continue
+
+        if area > biggest_area:
+            biggest_area = area
+            x, y, w, h = cv2.boundingRect(contour)
+            panel = image[y:y+h, x:x+w]
+    
+    panel = cv2.resize(panel, IMG_SIZE)
+
+    return panel
     
 
 def main():
-    image = single_image_loader("C:/Users/Philip Haugen/solarscan/data/images/Clean/Clean (25).jpg")
 
-    preprocessed_image = prep(image)
-    edges = detect_edges(preprocessed_image)
-    enhanced_edges = enhance_edges(edges)
-    contours = find_contours(enhanced_edges, image)
-  
-    cv2.imshow("Contours", contours)
+    image = single_image_loader("src/../data/images/Dusty/Dust (90).jpg")
+    panel = isolate_panel(image)
+
+    cv2.imshow("", panel)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+        
 
 if __name__ == "__main__":
     main()

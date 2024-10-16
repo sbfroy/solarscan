f7d1db498c2a5ed0d40e06c57f7efa1855775d1d
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-10-16 20:28:33 +0200

    pretty good panel separation

--- src/panel_separation.py
+++ src/panel_separation.py
@@ -1,10 +1,18 @@
-import numpy as np
 import cv2
+import os
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
 
 from data_loader import single_image_loader
 from preprocessing import prep
 from config import IMG_SIZE
 
+hsv_range = {
+    'h': (70, 160),        
+    's': (0, 70), 
+    #'v': (0, 100)       
+}
 
 def detect_edges(preprocessed_image):
     """
@@ -32,40 +40,92 @@ def find_contours(edges, image):
     contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
     return contours
     
-def isolate_panel(image):
+def color_check(image, contour, threshold):
     """
-    The pipeline for panel isolation.
+    Checks the standard deviation of the color in the contour. If the standard deviation is below the threshold, 
+    the area is considered uniform in color.
+        
+    Returns:
+        bool: True if the standard deviation is below the threshold, False otherwise.
+    """
+    mask = np.zeros(image.shape[:2], np.uint8)
+    cv2.drawContours(mask, [contour], -1, 255, thickness=-1) 
+    masked_image = cv2.bitwise_and(image, image, mask=mask)
+    pixels = masked_image[mask == 255]
+    _, stddev = cv2.meanStdDev(pixels)
+    return np.all(stddev < threshold)
+
+def color_range_check(image, contour, hsv_range):
+    """
+    Checks if the contours mean hsv value is within hsv_range (panel range).
+    
+    Returns:
+        bool: True if true, False otherwise.
+    """
+
+    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    cv2.drawContours(mask, [contour], -1, 255, thickness=-1)  # Fill the contour area
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    masked_image = cv2.bitwise_and(hsv_image, hsv_image, mask=mask)
+
+    mean_hsv = np.mean(masked_image[mask == 255], axis=0)
+
+    #print("Mean HSV: ", mean_hsv)
+
+    hue_in_range = hsv_range['h'][0] <= mean_hsv[0] <= hsv_range['h'][1]
+    sat_in_range = hsv_range['s'][0] <= mean_hsv[1] <= hsv_range['s'][1]
+    
+    return hue_in_range and sat_in_range  
+
+def isolate_panels(image):
+    """
+    The whole pipeline for panel isolation.
 
     Args: 
         image (): Unprocessed image from data_loader.py
 
     Returns:
-        numpy.ndarray: The isolated panel.
-
+        numpy.ndarray: Currently returns allt the isolated panels.
     """
     preprocessed_image = prep(image)
     edges = detect_edges(preprocessed_image)
     contours = find_contours(edges, image)
     sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
-    panel = None
+    panels = []
 
-    x, y, w, h = cv2.boundingRect(sorted_contours[1]) # ignores the largest contour (the whole image)
-    panel = image[y:y+h, x:x+w] # crops the panel from the image
-    panel = cv2.resize(panel, IMG_SIZE) 
+    max_area = cv2.contourArea(sorted_contours[0])
 
-    return panel
+    for contour in tqdm(sorted_contours):
+        x, y, w, h = cv2.boundingRect(contour)
+        area = cv2.contourArea(contour)
+
+        if area > 0.1 * max_area and area < 0.95 * max_area:
+            # Ignores the largest contour (the whole image)
+            if color_check(image, contour, 40) and color_range_check(image, contour, hsv_range): # Check if the area has traditional panel color
+                panel = image[y:y+h, x:x+w]
+                panels.append(panel)
+
+    return panels
     
 
 def main():
-    # dust (90).jpg
-    image = single_image_loader("src/../data/images/Dusty/Dust (90).jpg")
-    panel = isolate_panel(image)
 
-    cv2.imshow("", panel)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-        
+    base_dir = os.path.dirname(__file__)
+    img_path = Path(base_dir).parent / "data/images/Dusty/dust (90).jpg"
+
+    # dust (90).jpg
+    # Dust (130).jpg
+    image = single_image_loader(img_path)
+    panels = isolate_panels(image)
+
+    for panel in panels:
+        cv2.imshow("", panel)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
+    

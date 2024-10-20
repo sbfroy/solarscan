c99502797e26404310b33dd35b60b9cc1cb44059
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-10-20 13:15:57 +0200

    added test_step to model

--- src/model.py
+++ src/model.py
@@ -4,14 +4,12 @@ from torchvision import models
 import pytorch_lightning as pl
 import torch.nn.functional as F
 
-from config import IMG_SIZE
-
-# TODO: Since little data is available, maybe use k-fold cross validation.
+from config import LEARING_RATE
 
 
-class solarPanelClassifier(pl.LightningModule):
+class SOLARSCANMODEL(pl.LightningModule):
     def __init__(self, num_classes):
-        super(solarPanelClassifier, self).__init__()
+        super(SOLARSCANMODEL, self).__init__()
         self.model = models.resnet50(pretrained=True)
         # Replace the final layer with our number of classes
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes) 
@@ -35,7 +33,15 @@ class solarPanelClassifier(pl.LightningModule):
         acc = (torch.argmax(y_hat, dim=1) == y).float().mean()
         self.log('val_loss', loss, prog_bar=True)
         self.log('val_acc', acc, prog_bar=True)
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = (torch.argmax(y_hat, dim=1) == y).float().mean()
+        self.log('test_loss', loss, prog_bar=True)
+        self.log('test_acc', acc, prog_bar=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        optimizer = torch.optim.Adam(self.parameters(), lr=LEARING_RATE)
         return optimizer

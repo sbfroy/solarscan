a4bc52fc0139a0678ddb2b01962ea4e6f1f6de41
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-10-27 18:25:14 +0100

    new weights

--- src/test_SOLARSCANMODEL.py
+++ src/test_SOLARSCANMODEL.py
@@ -8,7 +8,7 @@ import torch
 base_dir = Path(os.getcwd())
 
 from model import SOLARSCANMODEL
-from config import IMG_SIZE, BATCH_SIZE, LEARNING_RATE
+from config import IMG_SIZE, BATCH_SIZE, LEARNING_RATE, LR_PATIENCE, LR_FACTOR
 
 transform = transforms.Compose([
                 transforms.Resize(IMG_SIZE),
@@ -26,10 +26,14 @@ dataloaders = {
 class_names = image_datasets['test'].classes
 num_classes = len(class_names)
 
-model = SOLARSCANMODEL(num_classes, learning_rate=LEARNING_RATE
-)
+model = SOLARSCANMODEL(
+    num_classes, 
+    learning_rate=LEARNING_RATE,
+    patience=LR_PATIENCE,
+    factor=LR_FACTOR
+    )
 
-model.load_state_dict(torch.load(base_dir / 'src/checkpoints/SOLARSCANMODEL_RESNET50_weights_v5.pth', map_location=torch.device('cpu')))
+model.load_state_dict(torch.load(base_dir / 'src/checkpoints/SOLARSCANMODEL_RESNET50_weights_v6.pth', map_location=torch.device('cpu')))
 model.eval()
 
 trainer = pl.Trainer()

57935504ca4a6ccb3347c1b32e855b305b0ed197
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-10-30 14:47:08 +0100

    single image predict

---
+++ src/predict.py
@@ -0,0 +1,56 @@
+import torch
+from torchvision import transforms
+import os
+from pathlib import Path
+import cv2
+from PIL import Image
+
+from data_loader import single_image_loader
+from preprocessing import prep
+from panel_separation import isolate_panels
+
+from model import SOLARSCANMODEL
+from config import IMG_SIZE, LEARNING_RATE, LR_PATIENCE, LR_FACTOR, CLASS_NAMES
+
+base_dir = os.path.dirname(__file__)
+
+transform = transforms.Compose([
+                transforms.Resize(IMG_SIZE),
+                transforms.ToTensor()
+            ])
+
+num_classes = len(CLASS_NAMES)
+model = SOLARSCANMODEL(
+    num_classes=num_classes, 
+    learning_rate=LEARNING_RATE,
+    patience=LR_PATIENCE,
+    factor=LR_FACTOR
+)
+
+model.load_state_dict(torch.load(Path(base_dir) / 'checkpoints/SOLARSCANMODEL_RESNET50_weights_v6.pth', map_location=torch.device('cpu')))
+model.eval()
+
+img_path = Path(base_dir).parent / "data/images/val/Clean/Clean (17).jpg"
+image = single_image_loader(img_path)
+panels = isolate_panels(image)
+
+cv2.imshow("", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+if panels:
+    image = panels[0] 
+else: 
+    print('No panels found.')
+    print('Showing model whole picture.')
+
+image = Image.fromarray(image) # Ensure it is a PIL image
+image = transform(image).unsqueeze(0)
+
+with torch.no_grad():
+    output = model(image)
+    _, predicted = torch.max(output, 1)
+
+predicted = CLASS_NAMES[predicted.item()]
+
+print(f'I think: {predicted}')

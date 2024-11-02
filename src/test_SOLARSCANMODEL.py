fc94d679108ee2f2d1fc4cfa717e01d6a2af7e36
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-11-02 13:35:15 +0100

    test update

--- src/test_SOLARSCANMODEL.py
+++ src/test_SOLARSCANMODEL.py
@@ -1,14 +1,14 @@
+import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from pathlib import Path
 import pytorch_lightning as pl
 import os
-import torch
 
 base_dir = Path(os.getcwd())
 
 from model import SOLARSCANMODEL
-from config import IMG_SIZE, BATCH_SIZE, LEARNING_RATE, LR_PATIENCE, LR_FACTOR, CLASS_NAMES_2
+from config import *
 
 transform = transforms.Compose([
                 transforms.Resize(IMG_SIZE),
@@ -23,12 +23,13 @@ dataloaders = {
     'test': DataLoader(image_datasets['test'], batch_size=BATCH_SIZE, shuffle=False)
 }
 
+num_classes = len(CLASS_NAMES_2)
 model = SOLARSCANMODEL(
-    num_classes=len(CLASS_NAMES_2), 
+    num_classes=num_classes, 
     learning_rate=LEARNING_RATE,
     patience=LR_PATIENCE,
     factor=LR_FACTOR
-    )
+)
 
 model.load_state_dict(torch.load(base_dir / 'src/checkpoints/SOLARSCANMODEL_weights_2classes.pth', map_location=torch.device('cpu')))
 model.eval()

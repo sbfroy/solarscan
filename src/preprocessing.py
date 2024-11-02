40619caead700eb378724ba5c3dc56f6d4459a7e
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2024-11-02 11:30:40 +0100

    preprocessing = done

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,7 @@
 import cv2
 
-from data_loader import single_image_loader
-
 def prep(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
     blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-    #equalized = cv2.equalizeHist(blurred) # improves contrast
     return blurred
     
